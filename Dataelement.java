cb60fe913953c1bfb37c5771c9323c21cdf2108d
Janick Braun <57684270+janickbraun@users.noreply.github.com>
2022-11-28 14:39:38 +0100

    Delete Dataelement.java

--- Dataelement.java
+++
@@ -1,7 +0,0 @@
-interface Dataelement
-{
-    public String getKey();
-    public boolean isSameKey(String value);
-    public boolean isKeyHigher(String value);
-    public void printInformation();
-}
